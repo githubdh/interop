d194c3f2d3029b26356e5945d6173c9b004ca805
Timothy Woo (╯°□°)╯︵ ┻━┻ <timwoo@microsoft.com>
2018-07-04 01:41:54 +0000

    Updated build-hello.cmd

--- build-hello.cmd
+++ build-hello.cmd
@@ -1,3 +1,3 @@
-call "%VSCOMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd" -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0
 
 cl /EHsc helloworld.cpp
