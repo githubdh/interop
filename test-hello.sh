e893b19e451e0fc3a5a419bacddab65af964f5df
Emma Smith <Emma.Smith@microsoft.com>
2018-07-06 22:38:34 +0000

    Updated test-hello.sh

--- test-hello.sh
+++ test-hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-#echo "starting tests"
-echo "Testing hello world!"
+echo "Starting tests - run HelloWorld from Parent Build Artifacts"
 # execute test
+$(System.ArtifactsDirectory)/drop/linux/outputs/build/HelloWorld
 # read the result from test/result and return code so that build succeeds or fails
-#echo "ending tests"
+echo "Reading results of test"
