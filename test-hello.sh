c15ea1d33410e1553c18124f2fedc7c4e2929dc6
Dinesh Haridas <dineshh@microsoft.com>
2018-07-06 21:43:20 +0000

    Updated test-hello.sh

--- test-hello.sh
+++ test-hello.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash 
 #echo "starting tests"
 echo "Testing hello world!"
 # execute test
-# read the result from test/results
+# read the result from test/result and return code so that build succeeds or fails
 #echo "ending tests"
